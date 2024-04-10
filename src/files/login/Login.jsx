cb18a28312c22b897ae56b0c2dd7c332f9a5a233
camkeshav <pkeshav619@gmail.com>
2024-04-10 11:32:09 +0530

    Initial

---
+++ src/files/login/Login.jsx
@@ -0,0 +1,117 @@
+import {React, useState} from 'react';
+import {Link} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+// import { toast,ToastContainer } from "react-toastify"
+import toast, { Toaster } from 'react-hot-toast';
+import "react-toastify/dist/ReactToastify.css"
+
+const Login=() =>{
+  const navigate=useNavigate()
+    const [inputs, setInputs] = useState({
+      username:"",
+      password:"",
+    });
+
+    const [password,setPassword] = useState('');
+    const [username,setUsername] = useState('');
+
+    const handleChange = (e) =>{
+      setInputs((previousState) => ({
+        ...previousState,
+        [e.target.name]: e.target.value,
+      }));
+    };
+
+    const sendRequest = async () => {
+      // add your api endpoint for login 
+      console.log(username,password)
+    try {
+      const response = await fetch(`http://localhost:8000/api/signinuser?username=${username}&password=${password}`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      let data=await response.json()
+      if(response.ok){
+        console.log(data.message);  
+        localStorage.setItem("isActiveUser", true)
+        if(localStorage.getItem("isActiveUser")){
+          toast.success(data.message)
+          navigate("/home")      
+        }
+      }
+      else
+      {
+        toast.error(data.message)
+        console.log(data.message);
+      }
+    } 
+    catch(error) {
+      console.error('Error :',error)
+    }
+    };
+  
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      console.log(inputs);
+      sendRequest();
+  };
+  return (
+    <>
+    <div><Toaster
+  position="top-right"
+  reverseOrder={false}
+/></div>
+    <div className="bg-black h-screen w-screen">
+    <div className="flex justify-center items-center h-screen ">
+      <div className="max-w-md w-full px-6 py-8">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-white">Emotunes</h1>
+        </div>
+        <div className="bg-gray-800 rounded-lg shadow-lg p-8">
+          <h2 className="text-2xl mb-4 font-semibold text-white text-center">Sign In</h2>
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div>
+              <input
+                type="username"
+                placeholder="Enter Your Username"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800 text-white"
+                name="username"
+                // value={inputs.username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Enter Your Password"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800 text-white"
+                name="password"
+                // value={inputs.password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-3 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            >
+              Sign In
+            </button>
+          </form>
+          <div className="mt-4 text-center">
+            <span className="text-gray-400">New to EmoTunes? </span>
+            <Link to="/register" ><b className="text-blue-600 cursor-pointer">Sign up Now!</b></Link>
+  
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </>
+  );
+}
+export default Login;
---
+++ src/files/register/Register.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useRef } from 'react';
+import { useNavigate } from "react-router-dom"
+import toast, { Toaster } from 'react-hot-toast';
+import "react-toastify/dist/ReactToastify.css"
+import Navbar from '../navbar/Navbar';
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const userNameRef = useRef();
+  const navigate=useNavigate()
+  const sendRequest = async () => {
+    // add your api endpoint for login 
+    console.log(userName,password)
+  try {
+    const response = await fetch(`http://localhost:8000/api/register?username=${userName}&email=${email}&password=${password}`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ userName, email, password }),
+    });
+    let data=await response.json()
+    if(response.ok){
+      console.log(data.message);
+      console.log("Login Accepted");
+      navigate('/Login');
+    }
+    else
+    {
+      toast.error(data.message)
+    }
+  } 
+  catch(error) {
+    console.error('Error :',error)
+  }
+  };
+
+  const handleStart = () => {
+    setUserName(userNameRef.current.value);
+    setEmail(emailRef.current.value);
+    setPassword(passwordRef.current.value);
+    sendRequest();
+  }
+
+
+  return (
+    <>
+    <div><Toaster
+  position="top-right"
+  reverseOrder={false}
+/></div>
+    <div className="bg-black min-h-screen flex justify-center items-center">
+      <div className="max-w-md w-full px-6 py-8 rounded-lg shadow-lg">
+        <div className="text-center mb-8">
+          <h1 className="text-3xl font-bold text-white">Get To know your emotions</h1>
+          <h2 className="text-xl font-semibold text-white">Ready to roll?</h2>
+          <p className="text-white">Enter your email to create or register</p>
+        </div>
+        <div className='bg-gray-800 rounded-lg shadow-lg p-8'>
+        <h2 className="text-2xl mb-4 font-semibold text-white text-center">Sign Up</h2>
+        <div className='space-y-4'>
+        <div className="flex flex-col gap-4 items-center">
+            <input
+              type="username"
+              placeholder="User Name"
+              ref={userNameRef}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800 text-white"
+            />
+            <input
+              type="email"
+              placeholder="Email Address"
+              ref={emailRef}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800 text-white"
+            />
+            <input
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800 text-white"
+            />
+
+            <button
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              onClick={handleStart}
+            >
+              Get Started
+            </button>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+
+    </>
+  );
+}
